3841b012cb3a170541c32c790d6e8fba53c86f18
moses-ramoeletsi <mramoeletsi14@gmail.com>
2025-03-03 14:26:39 +0200

    added database connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { dbConnection } from './dbConnection/dbConnection.js';
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {    
     res.send('Server is up and running');
 });
 
-app.listen(3000, () => {    
+app.listen(PORT, () => {   
+    dbConnection(); 
     console.log('Server is running on port 3000');
 });
