9064c7af586b80a12c5dfb3572253bbbaa0d8c6a
moses-ramoeletsi <mramoeletsi14@gmail.com>
2025-03-03 17:06:23 +0200

    updated the cors function

--- server/index.js
+++ server/index.js
@@ -11,7 +11,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['https://moses-ramoeletsi-portfolio.vercel.app', 'http://localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
