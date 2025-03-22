a6851515796602e421c845d8fcd96f588b437b7d
moses-ramoeletsi <mramoeletsi14@gmail.com>
2025-03-22 10:59:03 +0200

    updated the vit config file

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        // Replace with your actual deployed Vercel URL
-        target: "hhttps://moses-ramoeletsi-portfolio-server.vercel.app/",
+        target: "https://moses-ramoeletsi-portfolio-server.vercel.app",
         changeOrigin: true,
         secure: true,
         rewrite: (path) => path
